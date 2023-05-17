0cc7b20e5d451936cb18cb246e54b451b44c11fd
GeorgeSalu <george.salu10@gmail.com>
2023-05-17 16:02:47 -0300

    *ngClass

--- meu-primeiro-projeto/src/app/app.module.ts
+++ meu-primeiro-projeto/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { TitleComponent } from './title/title.component';
 import { DataBindigComponent } from './data-bindig/data-bindig.component';
 import { FormsModule } from '@angular/forms';
 import { DiretivasEstruturaisComponent } from './diretivas-estruturais/diretivas-estruturais.component';
+import { DiretivasAtributosComponent } from './diretivas-atributos/diretivas-atributos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
     DataBindigComponent,
-    DiretivasEstruturaisComponent
+    DiretivasEstruturaisComponent,
+    DiretivasAtributosComponent
   ],
   imports: [
     BrowserModule,
