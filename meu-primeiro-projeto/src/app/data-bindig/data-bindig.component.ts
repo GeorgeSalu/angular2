1ea2e8101640038eb303d5b6667ffafd85c50d56
GeorgeSalu <george.salu10@gmail.com>
2023-05-17 09:58:54 -0300

    property binding

--- meu-primeiro-projeto/src/app/data-bindig/data-bindig.component.ts
+++ meu-primeiro-projeto/src/app/data-bindig/data-bindig.component.ts
@@ -11,6 +11,10 @@ export class DataBindigComponent implements OnInit {
   public idade: number = 29;
   public maisUm: number = 1;
 
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://miro.medium.com/v2/resize:fit:720/format:webp/1*qgo4_MwETtxdspEqt12sNA.png";
+  public imgTitle: string = "imagem angular";
+
   constructor() { }
 
   ngOnInit(): void {
