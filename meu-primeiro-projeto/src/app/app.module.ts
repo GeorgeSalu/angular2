369e6d020c01d8541378ad1b7ae7244cfb29e0be
GeorgeSalu <george.salu10@gmail.com>
2023-05-17 11:39:12 -0300

    two-way bindig

--- meu-primeiro-projeto/src/app/app.module.ts
+++ meu-primeiro-projeto/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { DataBindigComponent } from './data-bindig/data-bindig.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DataBindigComponent } from './data-bindig/data-bindig.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
