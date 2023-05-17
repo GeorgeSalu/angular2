fe6f550aa6ccc1fdc2e2206a157d43b80ee0faa6
GeorgeSalu <george.salu10@gmail.com>
2023-05-17 11:18:21 -0300

    event binding

--- meu-primeiro-projeto/src/app/data-bindig/data-bindig.component.ts
+++ meu-primeiro-projeto/src/app/data-bindig/data-bindig.component.ts
@@ -15,9 +15,20 @@ export class DataBindigComponent implements OnInit {
   public imgSrc: string = "https://miro.medium.com/v2/resize:fit:720/format:webp/1*qgo4_MwETtxdspEqt12sNA.png";
   public imgTitle: string = "imagem angular";
 
+  public possition: {x: number, y: number} = {x: 0, y: 0};
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public alertaInfo(valor: MouseEvent) {
+    console.log(valor)
+  }
+
+  public mouseMoveTeste(valor: MouseEvent) {
+    this.possition.x = valor.offsetX;
+    this.possition.y = valor.offsetY;
   }
 
 }
