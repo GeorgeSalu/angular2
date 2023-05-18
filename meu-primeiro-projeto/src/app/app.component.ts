13c1cd148a7c6e0d3e78250647b41944bcbe652d
GeorgeSalu <george.salu10@gmail.com>
2023-05-18 11:26:36 -0300

    pipes

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <!--
   <app-diretivas-atributos>
     <h1>Aulas diretivas atributos</h1>
     <hr>
   </app-diretivas-atributos>
+  -->
+  <app-diretivas-atributos></app-diretivas-atributos>
   <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
