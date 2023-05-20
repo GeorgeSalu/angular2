253087901f4365030c20891735bf0b5362bc4b99
GeorgeSalu <george.salu10@gmail.com>
2023-05-20 15:40:41 -0300

    projeto app-todo - criando metodos para deletar uma task e para deletar todas as tasks

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -9,11 +9,24 @@ import { TaskList } from '../../model/task-list';
 export class TodoListComponent implements OnInit {
 
   public taskList: Array<TaskList> = [
+    { task: "Minha nova Task", checked: true },
+    { task: "Minha nova Task 2", checked: false }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Voce deseja realemtne deletar tudo ?");
+    if(confirm) {
+      this.taskList = [];
+    }
   }
 
 }
