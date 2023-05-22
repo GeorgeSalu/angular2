3ebf3231808174b5e91345fdc731fb5f51429bc6
GeorgeSalu <george.salu10@gmail.com>
2023-05-22 09:52:50 -0300

    service - injecao de dependencia

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -8,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
     <h1>Aulas diretivas atributos</h1>
     <hr>
   </app-diretivas-atributos>
-  -->
   <ng-template [ngIf]="getDados">
     <h1>{{ getDados.nome }}</h1>
     <h1>{{ getDados.idade }}</h1>
   </ng-template>
 
   <app-output (enviarDados)="setDados($event)"></app-output>
+  -->
+  <app-food-list></app-food-list>
   <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
