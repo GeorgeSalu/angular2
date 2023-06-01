8233cecf7d9a9fba56391ca273ed6284d95532ef
GeorgeSalu <george.salu10@gmail.com>
2023-06-01 11:59:33 -0300

    testando a unidade do componete

--- teste-unitario/src/app/shared/banking/banking.component.spec.ts
+++ teste-unitario/src/app/shared/banking/banking.component.spec.ts
@@ -22,4 +22,28 @@ describe('BankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`(U) getPoupanca(): should poupanca have 10`,() => {
+    expect(component.getPoupanca).toEqual(10);
+  })
+
+  it(`(U) getCarteira(): should carteira have 10`,() => {
+    expect(component.getPoupanca).toEqual(10);
+  })
+
+  it(`(U) getSacar(): should transfer poupanca from carteira`,() => {
+    component.setSacar('10');
+    fixture.detectChanges();
+
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  })
+
+  it(`(U) getDepositar(): should transfer carteira from poupanca`,() => {
+    component.setDepositar('50');
+    fixture.detectChanges();
+
+    expect(component.getCarteira).toEqual(0);
+    expect(component.getPoupanca).toEqual(60);
+  })
 });
