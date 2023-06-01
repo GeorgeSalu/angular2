8fbf2325f283c477b0432811877f99180fdda220
GeorgeSalu <george.salu10@gmail.com>
2023-06-01 16:28:46 -0300

    criando servico de investiments

--- teste-unitario/src/app/shared/investments/components/list/list.component.ts
+++ teste-unitario/src/app/shared/investments/components/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Investiments } from '../../model/investiments';
+import { ListInvestimentsService } from '../../services/list-investiments.service';
 
 @Component({
   selector: 'app-list',
@@ -27,9 +28,12 @@ export class ListComponent implements OnInit {
     }
   ]
 
-  constructor() { }
+  constructor(private listInvestimentsService: ListInvestimentsService) { }
 
   ngOnInit(): void {
+    this.listInvestimentsService.list().subscribe(
+      res => console.log(res)
+    )
   }
 
 }
