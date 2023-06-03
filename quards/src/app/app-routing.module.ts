267693ca1584a4ba9fa002349d7c996e18020345
GeorgeSalu <george.salu10@gmail.com>
2023-06-03 13:23:55 -0300

    separando em pastas

--- quards/src/app/app-routing.module.ts
+++ quards/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './shared/home/home.component';
-import { AccountComponent } from './shared/account/account.component';
+import { AccountComponent } from './shared/pages/account/account.component';
 import { CanActiveGuard } from './shared/guards/can-active.guard';
 import { CanDeactiveGuard } from './shared/guards/can-deactive.guard';
+import { HomeComponent } from './shared/pages/home/home.component';
 
 const routes: Routes = [
   {
