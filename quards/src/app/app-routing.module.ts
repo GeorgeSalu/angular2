7032ba8326179aee76790f4113f2fa58c0df6d7c
GeorgeSalu <george.salu10@gmail.com>
2023-06-03 12:38:03 -0300

    canActivate

--- quards/src/app/app-routing.module.ts
+++ quards/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/home/home.component';
 import { AccountComponent } from './shared/account/account.component';
+import { CanActiveGuard } from './shared/guards/can-active.guard';
 
 const routes: Routes = [
   {
@@ -10,7 +11,8 @@ const routes: Routes = [
   },
   {
     path: 'account',
-    component: AccountComponent
+    component: AccountComponent,
+    canActivate: [CanActiveGuard]
   }
 ];
 
