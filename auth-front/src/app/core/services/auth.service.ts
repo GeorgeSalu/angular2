415887d876061bdcd53d923ddb702c83fbadf0a3
GeorgeSalu <george.salu10@gmail.com>
2023-06-04 17:50:41 -0300

    projeto auth-front: hands on 11

--- auth-front/src/app/core/services/auth.service.ts
+++ auth-front/src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
@@ -9,12 +10,14 @@ export class AuthService {
 
   private url: string = "http://localhost:3000";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   public sign(payload: {email: string, password: string}): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payload).pipe(
-      map((data) => {
-        return console.log(data)
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
+      map((res) => {
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', res.token)
+        return this.router.navigate(['admin']);
       }),
       catchError((e) => {
         if(e.error.message) return throwError(() => e.error.message);
@@ -22,6 +25,11 @@ export class AuthService {
         return throwError(() => "No momento não estamos conseguindo validar esse dado.")
       })
     )
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
   }
 
 }
