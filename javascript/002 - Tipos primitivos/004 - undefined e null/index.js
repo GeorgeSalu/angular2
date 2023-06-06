a64734ffe6df57719a0002172be1dc1c1d121997
GeorgeSalu <george.salu10@gmail.com>
2023-06-06 14:56:29 -0300

    projeto javascript - undefined e null

---
+++ javascript/002 - Tipos primitivos/004 - undefined e null/index.js
@@ -0,0 +1,11 @@
+/**
+ * undefined = algo que não foi definido no momento
+ * null = algo que veio nulo
+ */
+
+let resposta;
+
+console.log(resposta);
+console.log(undefined);
+console.log(null);
+console.log(null === undefined)
