e3c6900774c7c8752ab30e289905f44cc643f7bc
GeorgeSalu <george.salu10@gmail.com>
2023-06-06 14:26:58 -0300

    projeto javascript - string

---
+++ javascript/002 - Tipos primitivos/001 - String/index.js
@@ -0,0 +1,10 @@
+console.log('aqui vai uma string')
+console.log("aqui vai uma string");
+console.log(`aqui vai uma string`)
+
+/**
+ * como podemos escrever uma string
+ * 'aqui vai uma string'
+ * "aqui vai uma string"
+ * `aqui vai uma string`
+ */
