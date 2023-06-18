f176c7ebb75d8cd7834553556c3534fa787266ed
GeorgeSalu <george.salu10@gmail.com>
2023-06-18 18:05:57 -0300

    projeto javascript - querySelectorAll

---
+++ javascript/010 - DOM/002 - Selecionando Elementos/002 - querySelectorAll/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const p = document.querySelectorAll("p");
+
+p.forEach((element, index) => {
+
+  if(index === 0) {
+    element.style.background = "blue";
+  }
+
+  if(index === 1) {
+    element.style.background = "red";
+  }
+
+});
