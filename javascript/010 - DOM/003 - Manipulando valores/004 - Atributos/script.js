0dad639640156246c0b65f925d4636e2a5756737
GeorgeSalu <george.salu10@gmail.com>
2023-06-20 12:04:22 -0300

    projeto javascript - atributos

---
+++ javascript/010 - DOM/003 - Manipulando valores/004 - Atributos/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const div = document.querySelector("div");
+div.setAttribute("id", "teste");
+div.setAttribute("class", "active");
+div.setAttribute("classe", "backgroud=red");
+
+console.log(div);
