e64e211205f1870b2a13c3f1e136f9563e3f0b80
GeorgeSalu <george.salu10@gmail.com>
2023-06-26 16:17:10 -0300

    typescript - alias

---
+++ typescript/002 - Tipos/007 - Aliases/index.ts
@@ -0,0 +1,13 @@
+type alpanumeric = string | number;
+
+let dados: alpanumeric;
+let dados2: alpanumeric;
+
+dados = 1;
+dados = "java";
+
+dados2 = 2;
+dados2 = "javascript";
+
+console.log(dados);
+console.log(dados2);
