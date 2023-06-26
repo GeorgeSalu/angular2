fdf610d8addc69e53c70ddab90661dea5ee3f71c
GeorgeSalu <george.salu10@gmail.com>
2023-06-26 11:23:59 -0300

    typescript - string, number, boolean

---
+++ typescript/002 - Tipos/001 - String - Number - Boolean/index.ts
@@ -0,0 +1,14 @@
+let pedido: string = "Pizza";
+pedido = "George"
+
+let numero: number = 123;
+numero = 321;
+
+let deuBom: boolean = false;
+deuBom = true;
+
+const fun = (value: string) => {
+  console.log(value)
+}
+
+fun("george");
