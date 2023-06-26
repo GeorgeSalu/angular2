b842d366aaa4a76b99c9dad43c44beeb373b01db
GeorgeSalu <george.salu10@gmail.com>
2023-06-26 11:33:40 -0300

    typescript - array e objetos

---
+++ typescript/002 - Tipos/002 - Array e Objeto/index.ts
@@ -0,0 +1,9 @@
+let array1: [string, string] = ["george", "teste"];
+let array2: Array<string | number> = ["george", "silva"];
+
+console.log(array1);
+console.log(array2);
+
+let obj1: {nome: string, sobrenome: string} = {nome: "george", sobrenome: "silva"}
+
+console.log(obj1)
