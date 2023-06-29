d65c4e142426a4b923c72564d6fd74484b02f5b3
GeorgeSalu <george.salu10@gmail.com>
2023-06-29 14:28:39 -0300

    typescript - modificadores de acesso

---
+++ typescript/003 - Classes/003 - Modificadores de acesso/index.ts
@@ -0,0 +1,20 @@
+class Pessoa {
+  public nome: string = "";
+  public idade: number = 0;
+
+  constructor(nome: string, idade: number){
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  public comer(comida: string) {
+    return `Ò ${this.nome} comeu ${comida}`;
+  }
+
+  public fezAniversario() {
+    return `Ò ${this.nome} fez tantos anos ${this.idade + 1}`;
+  }
+
+}
+
+const pessoa = new Pessoa("george", 30);
