f1852bedfd1581180ee96b59f6a3a15aca94c7a9
GeorgeSalu <george.salu10@gmail.com>
2023-07-02 15:10:23 -0300

    typescript - abstract

---
+++ typescript/003 - Classes/006 - Abstract/index.ts
@@ -0,0 +1,60 @@
+abstract class Pessoa {
+  protected nome: string = "";
+  protected idade: number = 0;
+
+  constructor(nome: string, idade: number) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  public comer(comida: string) {
+    return `O ${this.nome} comeu ${comida}`;
+  }
+
+  public fezAniversario() {
+    return `O ${this.nome} fez tantos anos ${++this.idade}`;
+  }
+
+  protected abstract profissao: string;
+  public abstract qualSuaProfissao(): string;
+  public abstract qualSeuSalario(salario: number): number;
+}
+
+class Maria extends Pessoa {
+  protected profissao: string = "Programadora";
+
+  constructor() {
+    super("Maria", 31);
+  }
+
+  public qualSuaProfissao(): string {
+    // Validações
+    return `Sua Profissão é ${this.profissao}`;
+  }
+
+  public qualSeuSalario(salario: number): number {
+    // Validações
+    return salario;
+  }
+}
+
+class Dener extends Pessoa {
+  protected profissao: string = "Programador";
+
+  constructor() {
+    super("Dener", 31);
+  }
+
+  public qualSuaProfissao(): string {
+    // Validações
+    return `Sua Profissão é ${this.profissao}`;
+  }
+
+  public qualSeuSalario(salario: number): number {
+    // Validações
+    return salario;
+  }
+}
+
+const maria = new Maria();
+console.log(maria.comer("Feijoada"));
