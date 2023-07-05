236be85d229775422139f0de38b9a8b3cd5b7cd4
GeorgeSalu <george.salu10@gmail.com>
2023-07-05 11:52:50 -0300

    ciclo de vida de componentes

--- projeto1/src/app/app.component.ts
+++ projeto1/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentInit, AfterViewInit {
   title = 'projeto1';
+
+  constructor() {
+    console.log("componente construtor")
+  }
+
+  ngAfterViewInit(): void {
+    console.log("")
+  }
+
+  ngAfterContentInit(): void {
+    console.log("componente ngAfterContenteInit")
+  }
+
+  ngOnDestroy(): void {
+    console.log("componente onDestroy")
+  }
+
+  ngDoCheck(): void {
+    console.log("componente doCheck")
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("componente OnChanges")
+  }
+
+  ngOnInit(): void {
+    console.log("componente onInit")
+  }
+
+  mudarTexto() {
+    this.title += "a";
+  }
+
 }
