18bef765bb3e09f71c9964416616c1e84b9d5b01
GeorgeSalu <george.salu10@gmail.com>
2023-07-05 12:16:30 -0300

    diretiva de decisao ngIf

--- projeto1/src/app/app.component.ts
+++ projeto1/src/app/app.component.ts
@@ -7,13 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
 
-  title = 'Bem vindo ao treinamento de angular';
-  imageUrl = "https://res.cloudinary.com/practicaldev/image/fetch/s--vp9p688o--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/e1y9n2k21pyu883ee938.jpg";
+  title = '';
 
   constructor() { }
 
-  chamarFuncao() {
-    console.log("esse é um click")
-  }
 
 }
