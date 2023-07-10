072b200f140e686cc5f57b353cab949d4fb7f336
GeorgeSalu <george.salu10@gmail.com>
2023-07-10 13:45:05 -0300

    buscando dados de produtos e listando na interface

--- projeto1/src/app/features/produto/services/produto.service.ts
+++ projeto1/src/app/features/produto/services/produto.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produtos } from '../models/produto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  private baseUrl: string = "https://viacep.com.br/ws/01001000/json/";
+  private baseUrl: string = "http://localhost:3000/";
 
   constructor(private http: HttpClient) { }
 
-  getCidadePeloCep() {
-    return this.http.get(this.baseUrl);
+  getProdutos(): Observable<Produtos> {
+    return this.http.get<Produtos>(`${this.baseUrl}produtos`)
   }
 
 }
