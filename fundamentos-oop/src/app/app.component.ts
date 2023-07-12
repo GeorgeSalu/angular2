2d0e95e9e4bd5eb1c26cb01181789f5b45d8da7a
GeorgeSalu <george.salu10@gmail.com>
2023-07-12 12:35:07 -0300

    modificadores de acesso/visibilidade

--- fundamentos-oop/src/app/app.component.ts
+++ fundamentos-oop/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MeuPrimeiroComponentComponent } from './meu-primeiro-component/meu-primeiro-component.component';
 import { MinhaPrimeiraClasse } from './fundamentos/classes/minhaPrimeiraClasse';
+import { MinhaSegundaClasse } from './fundamentos/classes/minhaSegundaClasse';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +15,11 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     let meuPrimeiraClasse = new MinhaPrimeiraClasse("Danilo", 30, "dev");
     let meuSegundaClasse = new MinhaPrimeiraClasse("Emerson", 20, "garcom");
+    let meuTerceiroObjeto = new MinhaSegundaClasse("rafa", 30, "analista");
 
     meuPrimeiraClasse.setNome("juliano")
+    console.log(meuTerceiroObjeto)
+    console.log(meuTerceiroObjeto.showNome())
 
     console.log(meuSegundaClasse.mostarNome());
     console.log(meuPrimeiraClasse);
---
+++ fundamentos-oop/src/app/fundamentos/classes/minhaSegundaClasse.ts
@@ -0,0 +1,9 @@
+import { MinhaPrimeiraClasse } from "./minhaPrimeiraClasse";
+
+export class MinhaSegundaClasse extends MinhaPrimeiraClasse {
+
+  showNome(): string {
+    return this.nome;
+  }
+
+}
