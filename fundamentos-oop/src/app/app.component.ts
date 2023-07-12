5a742c964afb3c756e7b19f7e43a2f32e0ad4e9b
GeorgeSalu <george.salu10@gmail.com>
2023-07-12 11:53:57 -0300

    como funciona a orientacao a objetos no angular

--- fundamentos-oop/src/app/app.component.ts
+++ fundamentos-oop/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MeuPrimeiroComponentComponent } from './meu-primeiro-component/meu-primeiro-component.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fundamentos-oop';
+
+
+  ngOnInit(): void {
+    let meuPrimeiroComponent = new MeuPrimeiroComponentComponent();
+
+    console.log(meuPrimeiroComponent)
+  }
+
 }
