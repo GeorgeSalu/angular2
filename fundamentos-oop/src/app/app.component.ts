e836e9b8887ba0487e8d0a766e190867b2a07489
GeorgeSalu <george.salu10@gmail.com>
2023-07-14 10:58:59 -0300

    trabalahndo no componente principal

--- fundamentos-oop/src/app/app.component.ts
+++ fundamentos-oop/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Quarto } from './interfaces/quarto.interface';
+import { HotelService } from './servicos/hotel.service';
+import { ClienteNormal } from './modelos/ClienteNormal';
+import { ClienteVIP } from './modelos/clienteVIP';
+import { QuartoSimples } from './modelos/quartoSimples';
+import { QuartoLuxo } from './modelos/quartoLuxo';
+import { Reserva } from './modelos/reserva';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +21,47 @@ export class AppComponent implements OnInit {
   tipoQuarto = "";
   quantidadeDias !: number;
 
+  constructor(public hotelService: HotelService) {}
+
+  criaCliente(): void {
+    if(this.tipoCliente === "normal") {
+      this.cliente = new ClienteNormal(this.cliente.nome, "Normal");
+      this.tipoQuarto = "simples";
+    } else if(this.tipoCliente === "vip" ) {
+      this.cliente = new ClienteVIP(this.cliente.nome, "VIP")
+      this.tipoQuarto = "de luxo";
+    }
+
+    this.hotelService.setCliente(this.cliente);
+    this.escolherQuarto();
+  }
 
   ngOnInit(): void {
 
   }
 
-  fazerRezerva(): void {
-    console.log(this.cliente.nome, this.tipoCliente, this.tipoQuarto, this.quantidadeDias)
+  escolherQuarto(): void {
+    if(this.tipoQuarto === "simples") {
+      this.quarto = new QuartoSimples();
+    } else if(this.tipoQuarto === "de luxo") {
+      this.quarto = new QuartoLuxo();
+    }
+
+    this.hotelService.setQuarto(this.quarto);
   }
+
+  fazerRezerva(): void {
+    this.criaCliente();
+
+    let cliente = this.hotelService.getCliente();
+    let quarto = this.hotelService.getQuarto();
+    let reserva = new Reserva(cliente, quarto, this.quantidadeDias);
+
+    this.hotelService.addReserva(reserva);
+
+    console.log(this.hotelService.getReservas())
+  }
+
+
 
 }
