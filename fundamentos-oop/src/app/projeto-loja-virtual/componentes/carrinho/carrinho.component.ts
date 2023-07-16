547d4f88584d64ee98feed097de83dde919248b2
GeorgeSalu <george.salu10@gmail.com>
2023-07-15 22:00:55 -0300

    trabalahndo no ts do componente carrinho de compras

--- fundamentos-oop/src/app/projeto-loja-virtual/componentes/carrinho/carrinho.component.ts
+++ fundamentos-oop/src/app/projeto-loja-virtual/componentes/carrinho/carrinho.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduto } from '../../interface/produto.interface';
+import { CarrinhoService } from '../../servicos/carrinho.service';
+import { TransacaoService } from '../../servicos/transacao.service';
+import { Transacao } from '../../modelos/transacao';
 
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
   styleUrls: ['./carrinho.component.css']
 })
-export class CarrinhoComponent {
+export class CarrinhoComponent implements OnInit {
+
+  produtosSelecionados: IProduto[] = [];
+  compraFinalizada = false;
+
+  constructor(
+    private carrinhoService: CarrinhoService,
+    private transacaoService: TransacaoService
+  ){}
+
+  ngOnInit(): void {
+    this.produtosSelecionados = this.carrinhoService.getProdutos();
+  }
+
+  calcularTotal(): number {
+    return this.carrinhoService.getTotal();
+  }
+
+  finalizarCompra(): void {
+    let total = this.calcularTotal();
+    let transacao = new Transacao(this.produtosSelecionados, total, new Date());
+    this.transacaoService.adicionaTransacao(transacao);
+  }
 
 }
