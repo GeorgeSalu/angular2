67ca5fd0cbbc497ca059dc4c3dbd4d7fe598b558
GeorgeSalu <george.salu10@gmail.com>
2023-07-15 23:36:24 -0300

    trabalhando no ts do componente de compras

--- fundamentos-oop/src/app/projeto-loja-virtual/componentes/compras/compras.component.ts
+++ fundamentos-oop/src/app/projeto-loja-virtual/componentes/compras/compras.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduto } from '../../interface/produto.interface';
+import { ITransacao } from '../../interface/transacao.interface';
+import { CarrinhoService } from '../../servicos/carrinho.service';
+import { TransacaoService } from '../../servicos/transacao.service';
 
 @Component({
   selector: 'app-compras',
   templateUrl: './compras.component.html',
   styleUrls: ['./compras.component.css']
 })
-export class ComprasComponent {
+export class ComprasComponent implements OnInit {
+
+  produtosDoCarrinho: IProduto[] = [];
+  transacoes: ITransacao[] =[];
+
+  constructor(
+    private carrinhoService: CarrinhoService,
+    private transacaoService: TransacaoService
+  ){}
+
+  ngOnInit(): void {
+    this.produtosDoCarrinho = this.carrinhoService.getProdutos();
+    this.transacoes = this.transacaoService.getTransacoes();
+  }
+
+  get getTransacoes(): ITransacao[] {
+    return this.transacoes;
+  }
 
 }
