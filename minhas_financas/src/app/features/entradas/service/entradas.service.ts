8fd5176afddf8ef009d5d50bbe947fb2438a8c9d
GeorgeSalu <george.salu10@gmail.com>
2023-07-20 14:00:48 -0300

    instalando a lib dayjs

--- minhas_financas/src/app/features/entradas/service/entradas.service.ts
+++ minhas_financas/src/app/features/entradas/service/entradas.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpBaseService } from 'src/app/shared/base/http-base.service';
+import { Entrada } from '../models/entrada.model';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,10 @@ export class EntradasService extends HttpBaseService{
 
   excluirEntrada(id: number): Observable<any> {
     return this.httpDelete(`${this.endpoint}/${id}`)
+  }
+
+  criarEntrada(payload: Entrada):Observable<any> {
+    return this.httpPost(`${this.endpoint}`, payload);
   }
 
 }
