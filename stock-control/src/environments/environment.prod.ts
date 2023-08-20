442ec08dc16675bddf3f19cab8f38d8211b0aca2
GeorgeSalu <george.salu10@gmail.com>
2023-08-20 11:08:14 -0300

    projeto stock-control - criando arquivos de envirnments pra cada ambiente

---
+++ stock-control/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const envinment = {
+  production: true,
+  API_URL: 'http://localhost:333'
+}
---
+++ stock-control/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const envinment = {
+  production: false,
+  API_URL: 'http://localhost:333'
+}
