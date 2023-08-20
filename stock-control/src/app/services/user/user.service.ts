784680b918a2b1f2b1e1000c6bf9c216fbc959df
GeorgeSalu <george.salu10@gmail.com>
2023-08-20 11:21:29 -0300

    projeto stock-control - criando services de criar usuario

---
+++ stock-control/src/app/services/user/user.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
+import { envinment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private API_URL = envinment.API_URL
+
+  constructor(private http: HttpClient) { }
+
+  signupUser(requestDatas: SignupUserRequest): Observable<SignupUserResponse> {
+    return this.http.post<SignupUserResponse>(
+      `${this.API_URL}/user`, requestDatas
+    )
+  }
+
+
+}
