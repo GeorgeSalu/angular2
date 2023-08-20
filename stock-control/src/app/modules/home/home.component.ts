4bde3d1dec7a7859b954b32482ccf5b01aea71de
GeorgeSalu <george.salu10@gmail.com>
2023-08-20 10:37:00 -0300

    projeto stock-control - implementando o formulario reativo login

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,15 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder){}
+
+  onSubmitLoginForm(): void {
+    console.log("dados dos formulario de login", this.loginForm.value)
+  }
 }
