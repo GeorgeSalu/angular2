220cfa7d366c7251358545bc8ce2d2714af1e279
GeorgeSalu <george.salu10@gmail.com>
2023-08-22 19:05:50 -0300

    projeto stock-control - implementando componente dashboard-home para consultar os produtos

--- stock-control/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ stock-control/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { GetAllProductsResponse } from './../../../../models/interfaces/products/response/GetAllProductsResponse';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: []
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit {
+
+  public productsList: Array<GetAllProductsResponse> = [];
+
+  constructor(private productService: ProductsService,
+              private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.getProductsDatas();
+  }
+
+  getProductsDatas(): void {
+    this.productService
+      .getAllProducts()
+      .subscribe({
+        next: (response) => {
+          if(response.length > 0) {
+            this.productsList = response;
+            console.log('dados dos produtos', this.productsList)
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 2500,
+          })
+        }
+      })
+  }
 
 }
