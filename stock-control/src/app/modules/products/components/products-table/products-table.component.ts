548a17a81e35a6136abbc72b36ffdda61ca6597a
GeorgeSalu <george.salu10@gmail.com>
2023-09-24 19:11:33 -0300

    projeto stock-control - criando o componente ProductsTableComponent

---
+++ stock-control/src/app/modules/products/components/products-table/products-table.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-products-table',
+  templateUrl: './products-table.component.html',
+  styleUrls: []
+})
+export class ProductsTableComponent {
+
+}
