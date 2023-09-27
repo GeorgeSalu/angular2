28088c2593a802a6966f9aac619b5764fc57bced
GeorgeSalu <george.salu10@gmail.com>
2023-09-27 18:12:25 -0300

    projeto stock-control - implementa service de categories e metodo de buscar categories

---
+++ stock-control/src/app/services/categories/categories.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { GetAllCategoriesResponse } from 'src/app/models/interfaces/categories/response/GetCategoriesResponse';
+import { envinment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  private API_URL = envinment.API_URL;
+  private JWT_TOKEN = this.coockie.get("USER_INFO");
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.JWT_TOKEN}`
+    })
+  }
+
+  constructor(
+    private http: HttpClient,
+    private coockie: CookieService
+  ) { }
+
+  getAllCategories(): Observable<Array<GetAllCategoriesResponse>> {
+    return this.http.get<Array<GetAllCategoriesResponse>>(
+      `${this.API_URL}/categories`
+    )
+  }
+
+}
