e7f783062c4b0b7ef12a876a7825392d582205aa
GeorgeSalu <george.salu10@gmail.com>
2023-09-27 20:08:44 -0300

    projeto stock-control - gerando componente e formulario de novo produto

--- stock-control/src/app/modules/products/components/product-form/product-form.component.ts
+++ stock-control/src/app/modules/products/components/product-form/product-form.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { GetAllCategoriesResponse } from 'src/app/models/interfaces/categories/response/GetCategoriesResponse';
+import { CategoriesService } from 'src/app/services/categories/categories.service';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: []
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit, OnDestroy {
+
+  public categoriesDatas: Array<GetAllCategoriesResponse> = [];
+  public selectedCategory: Array<{ name: string, code: string }> = [];
+  public addProductForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    description: ['', Validators.required],
+    category_id: ['', Validators.required],
+    amount: [0, Validators.required]
+  })
+
+  constructor(
+    private categoriesService: CategoriesService,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  getAllCategories(): void {
+    this.categoriesService.getAllCategories()
+    .subscribe({
+      next: (response) => {
+        if(response.length > 0) {
+          this.categoriesDatas = response;
+        }
+      }
+    })
+  }
+
+  handleSubmitAddProduct(): void {}
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
