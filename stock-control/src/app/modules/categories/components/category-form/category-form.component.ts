5bf46fbf8e5fb51f18a090b29258f7b2873fb6e3
GeorgeSalu <george.salu10@gmail.com>
2023-11-11 13:40:43 -0300

    projeto stock-control - implementando funcionalidade de adicionar categorias parte 1

--- stock-control/src/app/modules/categories/components/category-form/category-form.component.ts
+++ stock-control/src/app/modules/categories/components/category-form/category-form.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Subject } from 'rxjs';
+import { CategoryEvent } from 'src/app/models/enums/categories/CategoryEvent';
+import { EditCategoryAction } from 'src/app/models/interfaces/categories/event/EditCategoryAction';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
 
 @Component({
@@ -12,6 +14,14 @@ import { CategoriesService } from 'src/app/services/categories/categories.servic
 })
 export class CategoryFormComponent implements OnInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject();
+
+  public addCategoryAction = CategoryEvent.ADD_CATEGORY_ACTION;
+  public editCategoryAction = CategoryEvent.EDIT_CATEGORY_ACTION;
+
+  public categoryAction!: { event: EditCategoryAction };
+  public categoryForm = this.formBuilder.group({
+    name: ['', Validators.required]
+  })
 
   constructor(
     private ref: DynamicDialogRef,
@@ -23,6 +33,8 @@ export class CategoryFormComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
+
+  handleSubmitAddCategory(): void {}
 
   ngOnDestroy(): void {
     this.destroy$.next();
