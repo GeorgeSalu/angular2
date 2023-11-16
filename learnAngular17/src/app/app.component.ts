8ec51cb5b88bf930f53f825da3a0facaf57cfdf1
GeorgeSalu <george.salu10@gmail.com>
2023-11-16 20:49:52 -0300

    learnAngular17 - aprendendo utilizar nova sintaxe de template @for

--- learnAngular17/src/app/app.component.ts
+++ learnAngular17/src/app/app.component.ts
@@ -11,4 +11,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'learnAngular17';
+
+  userDatasList: Array<{
+    name: string;
+    age: number;
+    profession: string;
+    id: string;
+  }> = [
+    { age: 20, name: 'marcos', profession: 'software developer', id: '123' },
+    { age: 30, name: 'marcelo', profession: 'software developer', id: '456' },
+    { age: 40, name: 'carlos', profession: 'scrum master', id: '789' },
+    { age: 50, name: 'maria', profession: 'ux desings', id: '123' },
+  ]
+
 }
