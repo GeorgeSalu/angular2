44b4edcee104543ef6adfb70bf0a245c38570ca2
GeorgeSalu <george.salu10@gmail.com>
2023-11-18 10:41:49 -0300

    meu-primeiro-projeto-17 - conhecendo sobre componente

--- meu-primeiro-projeto-17/src/app/components/new-component/new-component.component.ts
+++ meu-primeiro-projeto-17/src/app/components/new-component/new-component.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NewComponet2Component } from '../new-componet-2/new-componet-2.component';
 
 @Component({
   selector: 'app-new-component',
   standalone: true,
-  imports: [],
+  imports: [NewComponet2Component],
   templateUrl: './new-component.component.html',
   styleUrl: './new-component.component.scss'
 })
 export class NewComponent {
-  
+
 }
---
+++ meu-primeiro-projeto-17/src/app/components/new-componet-2/new-componet-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-componet-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-componet-2.component.html',
+  styleUrl: './new-componet-2.component.scss'
+})
+export class NewComponet2Component {
+
+}
