30e08044e8d2e5097a0237c1175e9f19dfdb6ce7
GeorgeSalu <george.salu10@gmail.com>
2023-11-18 10:51:18 -0300

    meu-primeiro-projeto-17 - removendo o new-component-2

--- meu-primeiro-projeto-17/src/app/components/new-component/new-component.component.ts
+++ meu-primeiro-projeto-17/src/app/components/new-component/new-component.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { NewComponet2Component } from '../new-componet-2/new-componet-2.component';
 
 @Component({
   selector: 'app-new-component',
   standalone: true,
-  imports: [NewComponet2Component],
+  imports: [],
   templateUrl: './new-component.component.html',
   styleUrl: './new-component.component.scss'
 })
