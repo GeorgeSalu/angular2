85baad4f49e26ebc2c6e19f3e8ddc38285e4ab73
GeorgeSalu <george.salu10@gmail.com>
2023-11-20 18:36:37 -0300

    meu-primeiro-projeto-17 - control flow criando componente

--- meu-primeiro-projeto-17/src/app/app.component.ts
+++ meu-primeiro-projeto-17/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TamplateBindingComponent } from './components/template/tamplate-binding/tamplate-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, NewComponent, TamplateBindingComponent
-          ,TemplateVariablesComponent],
+          ,TemplateVariablesComponent, TemplateControlFlowComponent],
   styles: [`
     h1 {
       color: red
@@ -20,6 +21,7 @@ import { TemplateVariablesComponent } from './components/template/template-varia
     <h1>Curso de angular</h1>
     <app-tamplate-binding />
     <app-template-variables />
+    <app-template-control-flow />
   `,
 })
 export class AppComponent {
