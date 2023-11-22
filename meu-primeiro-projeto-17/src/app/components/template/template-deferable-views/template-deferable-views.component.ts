9a8751cab3943d8d0bc614757beb3c28c4ae67f4
GeorgeSalu <george.salu10@gmail.com>
2023-11-22 17:05:55 -0300

    meu-primeiro-projeto-17 - deferable views - criando componente template-deferable-views

---
+++ meu-primeiro-projeto-17/src/app/components/template/template-deferable-views/template-deferable-views.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-deferable-views',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-deferable-views.component.html',
+  styleUrl: './template-deferable-views.component.scss'
+})
+export class TemplateDeferableViewsComponent {
+
+}
