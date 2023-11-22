14ba6c37cbadde17952a7b222094ca0b5d3558c8
GeorgeSalu <george.salu10@gmail.com>
2023-11-22 17:55:55 -0300

    meu-primeiro-projeto-17 - deferable views - @defer when

--- meu-primeiro-projeto-17/src/app/components/template/template-deferable-views/template-deferable-views.component.ts
+++ meu-primeiro-projeto-17/src/app/components/template/template-deferable-views/template-deferable-views.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { NewComponent } from '../../new-component/new-component.component';
+import { Observable, delay, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-template-deferable-views',
   standalone: true,
-  imports: [NewComponent],
+  imports: [CommonModule, NewComponent],
   templateUrl: './template-deferable-views.component.html',
   styleUrl: './template-deferable-views.component.scss'
 })
 export class TemplateDeferableViewsComponent {
 
   public isTrue = false;
+
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3'
+  ]).pipe(delay(5000));
 
 }
