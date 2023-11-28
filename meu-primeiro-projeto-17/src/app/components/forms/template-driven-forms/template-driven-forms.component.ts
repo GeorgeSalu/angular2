5a608feac40aa47bf7ddefda31f0ff2778feedb9
GeorgeSalu <george.salu10@gmail.com>
2023-11-27 21:04:23 -0300

    meu-primeiro-projeto-17 - template driven forms - form select

--- meu-primeiro-projeto-17/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
+++ meu-primeiro-projeto-17/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './template-driven-forms.component.html',
   styleUrl: './template-driven-forms.component.scss'
 })
 export class TemplateDrivenFormsComponent {
+
+  public listComidas = signal<Array<{comida: string,preco: string}>>([
+    {
+      comida: 'x-salada', preco: 'R$ 10'
+    },
+    {
+      comida: 'x-bacon', preco: 'R$ 100'
+    },
+    {
+      comida: 'x-tudo', preco: 'R$ 150'
+    },
+  ]);
 
 }
