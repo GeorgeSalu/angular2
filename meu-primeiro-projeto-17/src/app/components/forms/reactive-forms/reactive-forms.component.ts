a8d3b7dc78b5a65a9464d2ec21a5950226f9b187
GeorgeSalu <george.salu10@gmail.com>
2023-11-29 17:26:16 -0300

    meu-primeiro-projeto-17 - reactive forms - forms control

--- meu-primeiro-projeto-17/src/app/components/forms/reactive-forms/reactive-forms.component.ts
+++ meu-primeiro-projeto-17/src/app/components/forms/reactive-forms/reactive-forms.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-forms.component.html',
   styleUrl: './reactive-forms.component.scss'
 })
 export class ReactiveFormsComponent {
 
+  name = new FormControl('dener');
 }
