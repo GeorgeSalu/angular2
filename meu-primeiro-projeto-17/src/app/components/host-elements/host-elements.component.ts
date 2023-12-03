767d993a869d7de43a4b268d233c1626e44d885d
GeorgeSalu <george.salu10@gmail.com>
2023-12-03 10:36:38 -0300

    meu-primeiro-projeto-17 - componentes avancados - host element

--- meu-primeiro-projeto-17/src/app/components/host-elements/host-elements.component.ts
+++ meu-primeiro-projeto-17/src/app/components/host-elements/host-elements.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-host-elements',
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './host-elements.component.scss'
 })
 export class HostElementsComponent {
+
+  @HostBinding('attr.class')
+  public class = 'vidafullstack';
+
+  @HostListener('document:keypress', ['$event'])
+  public updateHostListener(event: KeyboardEvent) {
+    console.log(event);
+  }
+
+  @HostListener('click', ['$event'])
+  public updateClick() {
+    alert('george')
+  }
 
 }
