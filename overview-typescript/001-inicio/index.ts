85c41aa447927734232146f75a3c6196cb899fa0
GeorgeSalu <george.salu10@gmail.com>
2023-12-04 18:29:55 -0300

    overview typescript - introducao

--- overview-typescript/001-inicio/index.ts
+++ overview-typescript/001-inicio/index.ts
@@ -1,1 +1,15 @@
+// typescript
+// npm install -g typescript
+// tsc --init
+
+// plugin: code runner
+// npm i -g ts-node
+// ctrl + alt + n
+
 console.log('typescript')
+
+const pedido = (pedido: string) => {
+  return `Deu bom o seu pedido ${pedido}`
+}
+
+console.log(pedido("123"))
