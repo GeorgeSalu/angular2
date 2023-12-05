811ffb575dce37d1cc29b74dbaea3aec4a22573d
GeorgeSalu <george.salu10@gmail.com>
2023-12-04 23:16:25 -0300

    overview typescript - module

--- overview-typescript/005-namespaces/module.ts
+++ overview-typescript/005-namespaces/module.ts
@@ -1,3 +1,12 @@
+/*
+  resumidamente os modulos em javascript ou typescript são uma forma de você 
+  compartilhar informações entre arquivos, dessa forma, todos os arquivos 
+  que realizam a importação desse modulo poderão utilizar e usufriir de suas
+  funções ou informações que foram expostas
+
+  import = importa informações de um class, namespaces, const , let e etc...
+  export = exporta para outras pessoas consumirem informações
+*/
 
 import { Pessoa1 } from "./pessoa-1"
 
