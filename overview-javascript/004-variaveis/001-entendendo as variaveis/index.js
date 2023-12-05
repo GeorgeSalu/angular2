ebf52bfda483e30761a8b7dd398cbf6af5e1a33d
GeorgeSalu <george.salu10@gmail.com>
2023-12-05 16:54:36 -0300

    overview-javascript - variaveis entendendo variaveis

--- overview-javascript/004-variaveis/001-entendendo as variaveis/index.js
+++ overview-javascript/004-variaveis/001-entendendo as variaveis/index.js
@@ -1,3 +1,9 @@
+/*
+  var // Ele é utilizado globalmente
+  let // Ele é gerando dentro de um escopo
+  const // Ele é gerando dentro de um escopo porém podemos armazenar dados apenas 1x
+*/
+
 var cachorro = "Kyara";
 console.log(cachorro);
 
