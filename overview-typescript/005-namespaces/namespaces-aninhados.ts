f12efe8f1f171ac5f3b149d6ef0f9335a82c3281
GeorgeSalu <george.salu10@gmail.com>
2023-12-04 22:59:25 -0300

    overview typescript - namespaces aninhados

---
+++ overview-typescript/005-namespaces/namespaces-aninhados.ts
@@ -0,0 +1,18 @@
+/*
+  os namespaces são uma froma especifica do typescript para organizar codigo,
+  namespaces são simplesmente objetos javascript nomeados no namespace global.
+  isso torna os namespaces uma construção muito simples de usar
+*/
+
+namespace Pessoa {
+  export namespace Pessoa1 {
+    export let nome = "joao"
+  
+    export const calc = () => {
+      return 1 + 1
+    }
+  
+  }
+}
+
+console.log(Pessoa.Pessoa1.nome)
