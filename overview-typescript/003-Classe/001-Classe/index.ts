62f969e4a4c20f7b213b030557debc96579537dc
GeorgeSalu <george.salu10@gmail.com>
2023-12-04 21:06:11 -0300

    overview typescript - classe

--- overview-typescript/003-Classe/001-Classe/index.ts
+++ overview-typescript/003-Classe/001-Classe/index.ts
@@ -1,3 +1,12 @@
+/**
+  uma classe typescript é uma estrutura de programação orientada a objetos
+  que possui um conjuto de proprieadades e metodos.
+
+  a classe é um modelo para criar objetos, permitindo que você defina 
+  uma estrutura com propriedades e comportamentos
+
+ */
+
 class Pessoa {
   nome: string;
   idade: number;
