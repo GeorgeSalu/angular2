960d30e0a5b20730563f3a1a2d7c276fd1f8d8d7
GeorgeSalu <george.salu10@gmail.com>
2023-12-05 14:30:15 -0300

    overview-javascript - tipos primitivos string

---
+++ overview-javascript/002-tipos primitivos/001-string/index.js
@@ -0,0 +1,10 @@
+/**
+ * como podemos escrever uma string
+ * 'aqui vai uma string'
+ * "aqui vai uma string"
+ * `aqui vai uma sstring`
+ */
+
+console.log("ola mundo");
+console.log('ola mundo');
+console.log(`ola mundo`);
