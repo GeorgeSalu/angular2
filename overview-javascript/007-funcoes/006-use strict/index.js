3d6938096537b188181bd4cdb77416230c9b081b
GeorgeSalu <george.salu10@gmail.com>
2023-12-06 15:27:14 -0300

    overview-javascript - funcoes - use strict

--- overview-javascript/007-funcoes/006-use strict/index.js
+++ overview-javascript/007-funcoes/006-use strict/index.js
@@ -1,3 +1,13 @@
+/**
+ * o strict mode eleminia alguns erros silenciosos
+ * que passariam batido do javascript e os faz emitir erros.
+ * 
+ * alem disso, essa forma corrige alguns erros que tornam o javascript 
+ * dificil de ser otimizado, então alguns vezes os codigos no modo estrito
+ * rodam mas otimizados e velozes do que os codigo no modo normal
+ * 
+ */
+
 "use strict";
 
 (() => {
