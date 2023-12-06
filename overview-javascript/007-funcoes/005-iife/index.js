fd8c0b1b9f46c8cedcc2bf3448f529ab01b1cbbc
GeorgeSalu <george.salu10@gmail.com>
2023-12-06 15:22:06 -0300

    overview-javascript - funcoes - iife

---
+++ overview-javascript/007-funcoes/005-iife/index.js
@@ -0,0 +1,7 @@
+// IIFE = immediately invoked function expression
+// IIFE = expressao de função invocada imediatamente
+
+(function(num1, num2){
+  const calc = num1 + num2;
+  console.log(calc);
+})(1,2);
