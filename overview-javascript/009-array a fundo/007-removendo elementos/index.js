67283c0160cb90e2630d503650e195e3b3e74980
GeorgeSalu <george.salu10@gmail.com>
2023-12-08 10:51:55 -0300

    overview-javascript - objetos a fundo - removendo elementos

--- overview-javascript/009-array a fundo/007-removendo elementos/index.js
+++ overview-javascript/009-array a fundo/007-removendo elementos/index.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * shifit = remove o primeiro elemento de um array e retorna esse elemento
+ * pop = remove o ultimo elemento de um array e retorna este elemento
+ * 
+ */
+
 let arr = [1,2,3,4,5];
 
 console.table(arr);
