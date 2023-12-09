6305eceab8bf044a8a0ebae0f649bdf7d12276c7
GeorgeSalu <george.salu10@gmail.com>
2023-12-09 12:03:03 -0300

    meu-primeiro-projeto-17 - remove import de ngOnit nao utilizado em AppComponent

--- meu-primeiro-projeto-17/src/app/app.component.ts
+++ meu-primeiro-projeto-17/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
