7b5f2d76a6e0bd359fcbbe57fa9e92818d9530c0
GeorgeSalu <george.salu10@gmail.com>
2024-08-03 11:40:00 -0300

    meu-primeiro-projeto-17 - lazy load component

--- meu-primeiro-projeto-17/src/app/app.routes.ts
+++ meu-primeiro-projeto-17/src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { SobreComponent } from './pages/sobre/sobre.component';
-import { ServicosPrestadosComponent } from './pages/servicos-prestados/servicos-prestados.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
