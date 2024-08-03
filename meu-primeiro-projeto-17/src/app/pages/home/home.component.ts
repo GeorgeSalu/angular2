ee8f7f4c924af50bd34b792479f401168d1846bf
GeorgeSalu <george.salu10@gmail.com>
2024-08-03 10:40:26 -0300

    meu-primeiro-projeto-17 - navegando entre paginas

--- meu-primeiro-projeto-17/src/app/pages/home/home.component.ts
+++ meu-primeiro-projeto-17/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
