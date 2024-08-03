05375f1cee67fe9734a21dedf3685277b1647a2a
GeorgeSalu <george.salu10@gmail.com>
2024-08-03 10:50:31 -0300

    meu-primeiro-projeto-17 - parametros

--- meu-primeiro-projeto-17/src/app/pages/servicos-prestados/servicos-prestados.component.ts
+++ meu-primeiro-projeto-17/src/app/pages/servicos-prestados/servicos-prestados.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servicos-prestados',
@@ -9,6 +10,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './servicos-prestados.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ServicosPrestadosComponent {
+export class ServicosPrestadosComponent implements OnInit {
+
+
+  #route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    console.log(this.#route.snapshot.params['id'])
+  }
 
 }
