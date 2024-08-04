21c8829d793a28a22256fcf1fa0e098e70aa4a05
GeorgeSalu <george.salu10@gmail.com>
2024-08-04 08:30:45 -0300

    meu-primeiro-projeto-17 - protecao de rotas - canActivatedChild

--- meu-primeiro-projeto-17/src/app/app.routes.ts
+++ meu-primeiro-projeto-17/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { canActivateChildGuard } from './guard/can-activate-child.guard';
 
 export const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./pages/curso.routes').then((r) => r.cursoRoutes),
+        canActivateChild: [canActivateChildGuard]
     },
     {
         path: '**',
