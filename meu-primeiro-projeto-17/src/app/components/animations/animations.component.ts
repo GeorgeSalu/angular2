94b16215e42e975c7aef53d782f62c26e8ace17b
GeorgeSalu <george.salu10@gmail.com>
2024-08-06 20:23:52 -0300

    meu-primeiro-projeto-17 - animacoes - trigger + state

--- meu-primeiro-projeto-17/src/app/components/animations/animations.component.ts
+++ meu-primeiro-projeto-17/src/app/components/animations/animations.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { state, style, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-animations',
@@ -8,7 +9,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './animations.component.html',
   styleUrl: './animations.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: []
+  animations: [
+    trigger('move-ball', [
+      state('move-left', style({
+        transform: 'scale(1) translateX(0) rotate(0deg)'
+      })),
+      state('move-right', style({
+        transform: 'scale(0.7) translateX(300px) rotate(360deg)'
+      }))
+    ])
+  ]
 })
 export class AnimationsComponent {
 
