b014641970e5f71b8db246d168df93b99518f992
GeorgeSalu <george.salu10@gmail.com>
2024-08-14 17:14:56 -0300

    projeto angular-basico - implementando formulario reativo

--- angular-basico/src/app/componente11/componente11.component.ts
+++ angular-basico/src/app/componente11/componente11.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-componente11',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './componente11.component.html',
   styleUrl: './componente11.component.css'
 })
 export class Componente11Component {
+
+  formulario = new FormGroup({
+    nome: new FormControl(''),
+    idade: new FormControl(null),
+    cidade: new FormControl(''),
+  })
 
 }
