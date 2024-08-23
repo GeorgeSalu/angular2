17ada185826669ece841ca550e63a25443df8732
GeorgeSalu <george.salu10@gmail.com>
2024-08-23 19:55:17 -0300

    projeto angular-basico - pipes parte 2

--- angular-basico/src/app/componente12/componente12.component.ts
+++ angular-basico/src/app/componente12/componente12.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MediaPipe } from '../pipe/media.pipe';
 
 @Component({
   selector: 'app-componente12',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MediaPipe],
   templateUrl: './componente12.component.html',
   styleUrl: './componente12.component.css'
 })
@@ -15,5 +16,13 @@ export class Componente12Component {
 
   // objeto
   obj: any = { 'nome': 'ralf', 'idade': 22 };
+
+  // vetor de objetos
+  alunos: any = [
+    { 'nome': 'ana', 'nota1': 8, 'nota2': 9 },
+    { 'nome': 'julio', 'nota1': 7, 'nota2': 8 },
+    { 'nome': 'leticia', 'nota1': 3, 'nota2': 2 },
+    { 'nome': 'ricardo', 'nota1': 7, 'nota2': 7 },
+  ]
 
 }
