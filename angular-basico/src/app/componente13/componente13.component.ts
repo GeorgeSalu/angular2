3e5d0c5147d9c12fb8f032490b778d8a6dffc070
GeorgeSalu <george.salu10@gmail.com>
2024-08-31 15:37:15 -0300

    projeto angular-basico - listagem de produtos

--- angular-basico/src/app/componente13/componente13.component.ts
+++ angular-basico/src/app/componente13/componente13.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Produto } from '../modelo/Produto';
+import { ProdutoService } from '../servico/produto.service';
 
 @Component({
   selector: 'app-componente13',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './componente13.component.html',
   styleUrl: './componente13.component.css'
 })
-export class Componente13Component {
+export class Componente13Component implements OnInit {
+
+  // vetor 
+  vetor: Produto[] = [];
+
+  // construtor
+  constructor(private servico: ProdutoService) { }
+
+  ngOnInit(): void {
+    this.selecionar()
+  }
+
+  // metodo para selecionar todos os produtos
+  selecionar() {
+    this.servico.selecionar().subscribe(retorno => { this.vetor = retorno })
+  }
 
 }
