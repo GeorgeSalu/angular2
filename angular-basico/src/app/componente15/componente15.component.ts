ca37476add016e96526ffc267c01503214d0239e
GeorgeSalu <george.salu10@gmail.com>
2024-09-02 18:34:23 -0300

    projeto angular-basico - criacao compoente15

---
+++ angular-basico/src/app/componente15/componente15.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente15',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente15.component.html',
+  styleUrl: './componente15.component.css'
+})
+export class Componente15Component {
+
+}
