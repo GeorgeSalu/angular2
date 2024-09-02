1ffeba7880fcc92cff37f8a2fbd95071b55f91fd
GeorgeSalu <george.salu10@gmail.com>
2024-09-02 18:35:15 -0300

    projeto angular-basico - criacao compoente15-tabela

---
+++ angular-basico/src/app/componente15-tabela/componente15-tabela.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente15-tabela',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente15-tabela.component.html',
+  styleUrl: './componente15-tabela.component.css'
+})
+export class Componente15TabelaComponent {
+
+}
