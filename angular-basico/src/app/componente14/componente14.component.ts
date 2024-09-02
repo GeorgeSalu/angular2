9f0acf5900da06c18d4dfec5507d762716e243d6
GeorgeSalu <george.salu10@gmail.com>
2024-09-02 18:20:16 -0300

    projeto angular-basico - criando componente

---
+++ angular-basico/src/app/componente14/componente14.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente14',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente14.component.html',
+  styleUrl: './componente14.component.css'
+})
+export class Componente14Component {
+
+}
