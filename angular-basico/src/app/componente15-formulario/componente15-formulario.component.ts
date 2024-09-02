d2311cb72557b80a3203329012eed1e665b60fcd
GeorgeSalu <george.salu10@gmail.com>
2024-09-02 18:34:52 -0300

    projeto angular-basico - criacao compoente15-formulario

---
+++ angular-basico/src/app/componente15-formulario/componente15-formulario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente15-formulario',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente15-formulario.component.html',
+  styleUrl: './componente15-formulario.component.css'
+})
+export class Componente15FormularioComponent {
+
+}
