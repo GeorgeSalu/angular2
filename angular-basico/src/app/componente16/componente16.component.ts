449613c4689d4ef57152aa0c9cb2b7d969bf91e6
GeorgeSalu <george.salu10@gmail.com>
2024-09-02 20:14:04 -0300

    projeto angular-basico - routerOutlet

--- angular-basico/src/app/componente16/componente16.component.ts
+++ angular-basico/src/app/componente16/componente16.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-componente16',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './componente16.component.html',
   styleUrl: './componente16.component.css'
 })
