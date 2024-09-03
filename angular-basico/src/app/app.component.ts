f46da1976307c9fb42659a1f2b39a01363c78e09
GeorgeSalu <george.salu10@gmail.com>
2024-09-02 22:06:15 -0300

    projeto angular-basico - criacao de componente para explorar teste unitarios

--- angular-basico/src/app/app.component.ts
+++ angular-basico/src/app/app.component.ts
@@ -16,11 +16,12 @@ import { Componente13Component } from "./componente13/componente13.component";
 import { Componente14Component } from "./componente14/componente14.component";
 import { Componente15Component } from "./componente15/componente15.component";
 import { Componente16Component } from "./componente16/componente16.component";
+import { Componente17Component } from "./componente17/componente17.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component, Componente07Component, Componente08Component, Componente09Component, Componente10Component, Componente11Component, Componente12Component, Componente13Component, Componente14Component, Componente15Component, Componente16Component],
+  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component, Componente07Component, Componente08Component, Componente09Component, Componente10Component, Componente11Component, Componente12Component, Componente13Component, Componente14Component, Componente15Component, Componente16Component, Componente17Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
