1e50287aa61aa26b4ef19f6b393d5b9687aec88b
GeorgeSalu <george.salu10@gmail.com>
2024-09-02 22:05:20 -0300

    projeto angular-basico - criacao de componente para explorar teste unitarios

---
+++ angular-basico/src/app/componente17/componente17.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente17',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente17.component.html',
+  styleUrl: './componente17.component.css'
+})
+export class Componente17Component {
+
+}
